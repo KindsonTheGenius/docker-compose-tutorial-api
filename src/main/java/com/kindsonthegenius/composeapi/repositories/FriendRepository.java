2696301ca25e299b04f2855d708de2c955dead09
KindsonTheGenius <Smart24*>
2021-05-21 09:16:15 +0200

    Final commit

--- src/main/java/com/kindsonthegenius/composeapi/repositories/FriendRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kindsonthegenius.composeapi.repositories;
-
-import com.kindsonthegenius.composeapi.models.Friend;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FriendRepository extends JpaRepository<Friend, Integer> {
-}
